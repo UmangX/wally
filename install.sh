aa4a6c81b05b2552b90a12219d891b6f05a4e4f7
Umang Patel <Agent.u99pro@gmail.com>
2022-06-22 16:19:13 +0530

    added install scripts and comments

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ref: https://askubuntu.com/a/30157/8698
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+if ! command -v feh &> /dev/null
+then
+    echo "feh is not installed use apt or pacman to install feh"
+    exit
+fi
+cp wally.py wally 
+sudo chmod +x wally 
+sudo mv wally /usr/bin/
--- wally.py
+++ wally.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python3
+
+#importing the libraries in the files
 from os import system
 from os import listdir
 from os import getlogin
 from random import randint
 
+#getting the username for the directories and path 
 username = getlogin()
+
+#assigning the name with username
 main_dir = '/home/'+username+'/Pictures'
+
+#using the list data structures for the storage and access of the file and images in the direstories 
 data_set = listdir(main_dir)
-pic_set = data_set[randint(0,len(data_set))]
+pic_set = data_set[randint(0,len(data_set)-1)]
+
+#using the system to execute a terminal command
 wally_command = 'feh --bg-fill '+main_dir+'/'+pic_set
 system(wally_command)
 
