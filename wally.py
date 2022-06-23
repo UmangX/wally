5b2a7d358bb5adf73d9ef46180cbef1d7ab3d735
Umang Patel <Agent.u99pro@gmail.com>
2022-06-23 13:38:49 +0530

    testing the gui

--- wally.py
+++ wally.py
@@ -5,12 +5,22 @@ from os import system
 from os import listdir
 from os import getlogin
 from random import randint
+from tkinter import filedialog
+from tkinter import *
+
+def if_dir_change():
+    root = Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+
+
 
 #getting the username for the directories and path 
 username = getlogin()
 
 #assigning the name with username
-main_dir = '/home/'+username+'/Pictures'
+#main_dir = '/home/'+username+'/Pictures'
+main_dir = folder_selected
 
 #using the list data structures for the storage and access of the file and images in the direstories 
 data_set = listdir(main_dir)
