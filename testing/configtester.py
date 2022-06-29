97b0f751ad19fc53b68417fb16a3dcc284a078be
Umang Patel <Agent.u99pro@gmail.com>
2022-06-30 00:43:42 +0530

    testing for config creation and retrieval

--- testing/configtester.py
+++ testing/configtester.py
@@ -3,12 +3,13 @@ from os import getlogin
 from os import path
 
 username = getlogin();
+config_location = '/home/'+username+'/.config/wally.ini'
 
-if path.exists('/home/'+username+'/.config/wally.ini') == False:
+if path.exists(config_location) == False:
     config = configparser.ConfigParser()
     print("select location")
     print("enter time in second or y for turing suffle off")
     time = input()
     config['setting'] = {'location' : '/home/'+username+'/Pictures' , 'timer' : time}
-    with open('wally.ini', 'w') as config_file:
+    with open(config_location, 'w') as config_file:
         config.write(config_file)
